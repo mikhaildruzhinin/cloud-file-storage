9bf5cdadbf28ed8f5917e8d996669d35036eb958
Mikhail Druzhinin <druzhininmy@gmail.com>
2023-10-01 01:58:07 +0300

    add IndexController

---
+++ src/main/java/ru/mikhaildruzhinin/storage/web/IndexController.java
@@ -0,0 +1,17 @@
+package ru.mikhaildruzhinin.storage.web;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@Controller
+@RequestMapping("/")
+public class IndexController {
+
+    @GetMapping
+    public String index() {
+        return "index";
+    }
+}
